cb37745bb932fc9155e7f7cf2b700ea43ce57e2c
Aparna Mishra <somap.mishra@gmail.com>
2024-08-13 19:45:45 +0100

    Weather and shops api backend/frontend done for inegration

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # OpenWeather API configuration
-api_key = 'c2408fd972ab0ccb7e3eeb5c3b0aad1b'
-places_api_key ='AIzaSyAknLDf9ltafB_7zNwW-UnnJ6V8NNAuDkg'
-google_maps_api_key='AIzaSyAknLDf9ltafB_7zNwW-UnnJ6V8NNAuDkg'
+api_key = 'your_key'
+places_api_key ='your_key'
+google_maps_api_key='your_key'
 
 base_url = 'https://api.openweathermap.org/data/2.5/forecast?'
 base_places_url = 'https://places.googleapis.com/v1/places:searchText'
@@ -9,5 +9,5 @@ base_places_url = 'https://places.googleapis.com/v1/places:searchText'
 # Database configuration
 DB_HOST = 'localhost'
 DB_USERNAME = 'root'
-DB_PASSWORD = 'Somasoma@15'
+DB_PASSWORD = 'your_password'
 DB_NAME = 'garden_app'
