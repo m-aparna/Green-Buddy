17a1447a1a051adbbb45ab352c79c3b8fb135d41
Aparna Mishra <somap.mishra@gmail.com>
2024-08-13 20:17:31 +0100

    changes on config file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # OpenWeather API configuration
-api_key = 'c2408fd972ab0ccb7e3eeb5c3b0aad1b'
-places_api_key ='AIzaSyArUb9_k-yKm3SdBTEMcsqShJ_xBMuDpX4'
-google_maps_api_key='AIzaSyArUb9_k-yKm3SdBTEMcsqShJ_xBMuDpX4'
+api_key = ''
+places_api_key =''
+google_maps_api_key=''
 
 base_url = 'https://api.openweathermap.org/data/2.5/forecast?'
 base_places_url = 'https://places.googleapis.com/v1/places:searchText'
@@ -9,5 +9,5 @@ base_places_url = 'https://places.googleapis.com/v1/places:searchText'
 # Database configuration
 DB_HOST = 'localhost'
 DB_USERNAME = 'root'
-DB_PASSWORD = 'Somasoma@15'
+DB_PASSWORD = ''
 DB_NAME = 'garden_app'
