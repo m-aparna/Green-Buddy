cd69d11f7b95e9fa7cbd0e38c4b8edd8f88a8157
Aparna Mishra <somap.mishra@gmail.com>
2024-08-22 21:09:38 +0100

    added comment in config

--- config.py
+++ config.py
@@ -9,14 +9,13 @@ plant_api_key = 'sk-88aa66a3807c54f5f6340'
 
 # OpenWeather API configuration
 
-weather_api_key = 'c2408fd972ab0ccb7e3eeb5c3b0aad1b'  # OpenWeatherMap API key
+weather_api_key = ''  # OpenWeatherMap API key
+base_weather_url = 'https://api.openweathermap.org/data/2.5/forecast?'  # Base url for openweathermap API
 
 # Places API configuration
 
-google_api_key = 'AIzaSyDG9gfVHTcfkkmnu3SzQhcDOJJYSnpMb9g'  # Google places API key,maps embed api key and geocoding api key are all same
-
-base_geocoding_url ="https://maps.googleapis.com/maps/api/geocode/json?address="
-base_weather_url = 'https://api.openweathermap.org/data/2.5/forecast?'  # Base url for openweathermap API
+google_api_key = ''  # Google places API key,maps embed api key and geocoding api key are all same
+base_geocoding_url ="https://maps.googleapis.com/maps/api/geocode/json?address="  # Google geocoding API url for validating location
 base_places_url = 'https://places.googleapis.com/v1/places:searchText'  # Base url for google places API
 
 
