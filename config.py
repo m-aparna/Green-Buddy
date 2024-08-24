8609bb5759bdd1e1a3d187f92dcc01a01f0948fc
Aparna Mishra <somap.mishra@gmail.com>
2024-08-24 22:12:02 +0100

    Removed API key from config and added current-day in weather html

--- config.py
+++ config.py
@@ -2,19 +2,19 @@
 secret_key = '_my secret key_'
 
 # YouTube API
-youtube_api_key = 'AIzaSyAOfu-uwILuNDI9n7TZscjP2CnC0Crm7zg'
+youtube_api_key = 'YOUR_API_KEY'
 
 # Plant API
 plant_api_key = 'sk-88aa66a3807c54f5f6340'
 
 # OpenWeather API configuration
 
-weather_api_key = ''  # OpenWeatherMap API key
+weather_api_key = 'YOUR_API_KEY'  # OpenWeatherMap API key
 base_weather_url = 'https://api.openweathermap.org/data/2.5/forecast?'  # Base url for openweathermap API
 
 # Places API configuration
 
-google_api_key = ''  # Google places API key,maps embed api key and geocoding api key are all same
+google_api_key = 'YOUR_API_KEY'  # Google places API key,maps embed api key and geocoding api key are all same
 base_geocoding_url ="https://maps.googleapis.com/maps/api/geocode/json?address="  # Google geocoding API url for validating location
 base_places_url = 'https://places.googleapis.com/v1/places:searchText'  # Base url for google places API
 
