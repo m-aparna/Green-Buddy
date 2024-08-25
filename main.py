5db44e1cebe2279ddafda2491362feebb7a1aa25
Ashie-03 <109604613+Ashie-03@users.noreply.github.com>
2024-08-25 12:22:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from app import create_app
 
 db_name = 'User.db'
 db_URI = f'sqlite:///{db_name}'
-# Create app using flask_app function
+
+# Create app using create_app function
 app = create_app(db_URI)
 
 # Run the app
